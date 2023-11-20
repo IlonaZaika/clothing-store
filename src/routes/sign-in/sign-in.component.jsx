5bc591125d1100525171d4b3a9dfb03fa7fc4c4c
Ilona Zaika <zaika.ilona.v@gmail.com>
2023-11-20 22:05:29 +0000

    Added sign in component and firebase utils.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,17 @@
+import { signInWIthGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWIthGooglePopup();
+    console.log(response);
+    const userDocRef = await createUserDocumentFromAuth(response.user);
+  }
+
   return (
-    <div>I am sign in component</div>
+    <div>
+      <div>I am sign in component</div>
+      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    </div>
   );
 }
 
---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,54 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdu3XfjZ91FiT5gPSlRInGrcH780iC6yc",
+  authDomain: "crown-store-db-b0044.firebaseapp.com",
+  projectId: "crown-store-db-b0044",
+  storageBucket: "crown-store-db-b0044.appspot.com",
+  messagingSenderId: "756555582079",
+  appId: "1:756555582079:web:8fb9c39cc8c84f7b3f4d4a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWIthGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = await doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('creating user error', error.message);
+    }
+  }
+
+  return userDocRef;
+};
