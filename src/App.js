2544d8a43b5a5d16a3838fe422a395340a129553
Ilona Zaika <zaika.ilona.v@gmail.com>
2023-12-12 22:41:39 +0000

    Implemented categories-preview.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
-
   return (
     <Routes>
-      <Route path='/' element={<Navigation />} >
+      <Route path='/' element={<Navigation />}>
         <Route index={true} element={<Home />} />
         <Route path='/auth' element={<Authentication />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='/shop/*' element={<Shop />} />
         <Route path='/checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
