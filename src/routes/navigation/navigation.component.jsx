725d6edac5703552f7eecb2d5fb80e332d34a917
Ilona Zaika <zaika.ilona.v@gmail.com>
2023-12-13 21:38:38 +0000

    Fixed styling.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
-import { auth, signOutUser } from '../../utils/firebase/firebase.utils';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
@@ -20,12 +20,20 @@ const Navigation = () => {
           <CrwnLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
-          <Link className='nav-link' to='/'>HOME</Link>
-          <Link className='nav-link' to='/shop'>SHOP</Link>
+          <Link className='nav-link' to='/'>
+            HOME
+          </Link>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
           {currentUser ? (
-            <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
           ) : (
-            <Link className='nav-link' to='/auth'>SIGN IN</Link>
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
           )}
           <CartIcon />
         </div>
@@ -34,6 +42,6 @@ const Navigation = () => {
       <Outlet />
     </>
   );
-}
+};
 
 export default Navigation;
